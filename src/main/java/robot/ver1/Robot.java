397736ff15c781db0f496804a6b326b0a07ea27b
Alex Uncevich <a.juncevich@gmail.com>
2015-11-09 23:03:31 +0500

    Add Getters and Setters.

--- src/main/java/robot/ver1/Robot.java
+++ src/main/java/robot/ver1/Robot.java
@@ -1,10 +1,65 @@
 package robot.ver1;
 
-public class Robot {
-    //Текущая координата Х.
-    int x =0;
-    //Текущая координата У.
-    int y = 0;
-    //Текущий курс в (градусах).
-    int course = 0;
+/**
+ *
+ */
+class Robot {
+    /**
+     * Текущая координата X.
+     */
+    private int x = 0;
+    /**
+     * Текущая координата У.
+     */
+    private int y = 0;
+    /**
+     * Текущий курс в (градусах).
+     */
+    private int course = 0;
+    /**
+     *
+     * @return x.
+     */
+    public int getX() {
+        return x;
+    }
+    /**
+     *
+     * @param x Текущая координата X.
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     *
+     * @return y.
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     *
+     * @param y Текущая координата У.
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     *
+     * @return course.
+     */
+    public int getCourse() {
+        return course;
+    }
+
+    /**
+     *
+     * @param course Текущий курс в (градусах).
+     */
+    public void setCourse(int course) {
+        this.course = course;
+    }
 }
