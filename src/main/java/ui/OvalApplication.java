10b829fcd35f0a6c517f8e6d0e08a4792b12e4d8
Alex <a.juncevich@gmail.com>
2015-11-10 13:12:07 +0500

    Add OvalApplication.

---
+++ src/main/java/ui/OvalApplication.java
@@ -0,0 +1,14 @@
+package ui;
+
+import javax.swing.*;
+
+/**
+ * Created by Juncevich on 10.11.2015.
+ */
+public class OvalApplication  {
+    public static void main(String[] args) {
+        OvalFrame ovalFrame = new OvalFrame();
+        ovalFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        ovalFrame.setVisible(true);
+    }
+}
