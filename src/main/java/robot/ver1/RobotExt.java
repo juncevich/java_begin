570e246f94f8524cca110686c17086568997b460
Alex <a.juncevich@gmail.com>
2015-11-10 09:31:32 +0500

    Add constructor and method "back" to calss RobotExt.

--- src/main/java/robot/ver1/RobotExt.java
+++ src/main/java/robot/ver1/RobotExt.java
@@ -3,5 +3,12 @@ package robot.ver1;
 /**
  * Created by Juncevich on 10.11.2015.
  */
-public class RobotExt {
+public class RobotExt extends Robot {
+    public RobotExt(double x, double y) {
+        super(x, y);
+    }
+
+    public void back(int distance){
+        forward(- distance);
+    }
 }
