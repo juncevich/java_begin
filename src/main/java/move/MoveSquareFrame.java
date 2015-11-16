75090a26ac5408e45823bf40e76dce8076d5a294
Alex <alexunc@mail.ru>
2015-11-16 22:27:05 +0500

    Finished MoveSquareFrame and SquareComponent.

--- src/main/java/move/MoveSquareFrame.java
+++ src/main/java/move/MoveSquareFrame.java
@@ -1,7 +1,37 @@
 package move;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.jar.JarFile;
+
 /**
  * Created by Alex on 16.11.2015.
  */
-public class MoveSquareFrame  {
+public class MoveSquareFrame extends JFrame {
+    public static final String UP = "UP";
+    public static final String DOWN = "DOWN";
+
+    public MoveSquareFrame() throws HeadlessException {
+        SquareComponent sc = new SquareComponent();
+// Кладем компонент для рисования квадрата
+        add(sc);
+// Создаем кнопку для посылки команды движения вверх
+        JButton btnUP = new JButton(UP);
+// Устанавливаем ей идентификатор, по которому сможем узнать эту кнопку
+        btnUP.setActionCommand(UP);
+// Устанавливаем ей слушатель - компонент для рисования квадрата
+        btnUP.addActionListener(sc);
+// Кладем кнопку на самый верх формы - на север
+        add(btnUP, BorderLayout.NORTH);
+// Создаем кнопку для посылки команды движения вниз
+        JButton btnDown = new JButton(DOWN);
+// Устанавливаем ей идентификатор, по которому сможем узнать эту кнопку
+        btnDown.setActionCommand(DOWN);
+// Устанавливаем ей слушатель - компонент для рисования квадрата
+        btnDown.addActionListener(sc);
+// Кладем кнопку на самый низ формы - на юг
+        add(btnDown, BorderLayout.SOUTH);
+// Устанавливаем координаты
+        setBounds(100, 100,400,400);
+    }
 }
