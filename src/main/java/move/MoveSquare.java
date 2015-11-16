5052b292915ff2df300947cd7149df300529589e
Alex <alexunc@mail.ru>
2015-11-16 22:31:54 +0500

    Realize up\down movement square.

---
+++ src/main/java/move/MoveSquare.java
@@ -0,0 +1,17 @@
+package move;
+
+import javax.swing.*;
+
+/**
+ * Created by Alex on 16.11.2015.
+ */
+public class MoveSquare {
+    public static void main(String[] args) {
+
+        MoveSquareFrame msf = new MoveSquareFrame();
+
+        msf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        msf.setVisible(true);
+    }
+}
