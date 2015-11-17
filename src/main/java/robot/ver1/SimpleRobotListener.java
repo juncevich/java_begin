893f5e00a1b21ce7db96bfcad4c1aaa2f9f74620
Alex <a.juncevich@gmail.com>
2015-11-17 09:47:11 +0500

    Remove Override in SimpleListener.

--- src/main/java/robot/ver1/SimpleRobotListener.java
+++ src/main/java/robot/ver1/SimpleRobotListener.java
@@ -4,12 +4,12 @@ package robot.ver1;
 public class SimpleRobotListener implements RobotListener
 {
 
-    @Override
+
     public void startMove(double x, double y)
     {
         System.out.println("Робот начал движение, координаты:" + x + "," + y);
     }
 
-    @Override
+
     public void endMove(double x, double y){ System.out.println("Робот закончил движение, координаты:" + x + "," + y); }
 }
