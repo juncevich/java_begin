976edf165c86ed6e0c06784a38f1f5d700b2924a
Alex <alexunc@mail.ru>
2015-11-17 23:25:53 +0500

    Add contactDAO.

--- src/main/java/contact/dao/ContactDAO.java
+++ src/main/java/contact/dao/ContactDAO.java
@@ -1,4 +1,15 @@
 package contact.dao;
 
-public class ContactDAO {
+import contact.entity.Contact;
+
+import java.util.List;
+
+public interface ContactDAO {
+    public Long addContact(Contact contact);
+
+    public void updateContact(Contact contact);
+
+    public void deleteContact(Long contactId);
+
+    public List<Contact> findContacts();
 }
