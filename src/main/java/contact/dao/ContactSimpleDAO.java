fc2c9c619daf7c70d569c39994bd648655315448
alex <a.juncevich@gmail.com>
2016-04-16 19:10:31 +0500

    Fix some errors.

--- src/main/java/contact/dao/ContactSimpleDAO.java
+++ src/main/java/contact/dao/ContactSimpleDAO.java
@@ -1,5 +1,28 @@
 package contact.dao;
 
 
-public class ContactSimpleDAO extends ContactDAO {
+import contact.entity.Contact;
+
+import java.util.List;
+
+class ContactSimpleDAO implements ContactDAO {
+    @Override
+    public Long addContact(Contact contact) {
+        return null;
+    }
+
+    @Override
+    public void updateContact(Contact contact) {
+
+    }
+
+    @Override
+    public void deleteContact(Long contactId) {
+
+    }
+
+    @Override
+    public List<Contact> findContacts() {
+        return null;
+    }
 }
