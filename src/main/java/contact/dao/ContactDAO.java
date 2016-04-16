0caa2cf3d9fe5d1b396e756b88573a74cda70af4
alex <a.juncevich@gmail.com>
2016-04-16 19:06:01 +0500

    Start cleaning the code.

--- src/main/java/contact/dao/ContactDAO.java
+++ src/main/java/contact/dao/ContactDAO.java
@@ -5,11 +5,11 @@ import contact.entity.Contact;
 import java.util.List;
 
 public interface ContactDAO {
-    public Long addContact(Contact contact);
+    Long addContact(Contact contact);
 
-    public void updateContact(Contact contact);
+    void updateContact(Contact contact);
 
-    public void deleteContact(Long contactId);
+    void deleteContact(Long contactId);
 
-    public List<Contact> findContacts();
+    List<Contact> findContacts();
 }
--- src/main/java/robot/ver1/RobotListener.java
+++ src/main/java/robot/ver1/RobotListener.java
@@ -5,7 +5,7 @@ package robot.ver1;
  */
 public interface RobotListener {
     // Метод будет вызываться в момент начала движения
-    public void startMove(double x, double y);
+    void startMove(double x, double y);
     // Метод будет вызываться в момент окончания движения
-    public void endMove(double x, double y);
+    void endMove(double x, double y);
 }
